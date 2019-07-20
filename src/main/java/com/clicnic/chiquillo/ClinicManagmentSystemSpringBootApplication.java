85667b49d6b6afe461df76255c47f2d67fb89e2f
Carlos <carlosjosechiquillolopez@gmail.com>
2019-07-20 16:35:15 -0600

    first commit in a secondary branch

---
+++ src/main/java/com/clicnic/chiquillo/ClinicManagmentSystemSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.clicnic.chiquillo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicManagmentSystemSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicManagmentSystemSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/clicnic/chiquillo/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.clicnic.chiquillo;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ClinicManagmentSystemSpringBootApplication.class);
+	}
+
+}
